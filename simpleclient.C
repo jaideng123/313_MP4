cbcc7a1a136c2c0309b694bab4bf09ba68a24e6a
jaideng123 <jaideng123@yahoo.com>
2015-04-16 16:42:19 -0500

    THIS IS ALL SO WRONG

--- simpleclient.C
+++ simpleclient.C
@@ -43,9 +43,12 @@ using namespace std;
 
 struct Arguments{
   char id;
+  int rep;
   BoundedBuffer* b;
   string channel;
-  vector<BoundedBuffer *> buffers;
+  BoundedBuffer * b1;
+  BoundedBuffer * b2;
+  BoundedBuffer * b3;
 };
 
 /*--------------------------------------------------------------------------*/
@@ -75,28 +78,26 @@ void *request(void *param){
 void *worker(void *param){
   Arguments* arg = (Arguments *)param;
   RequestChannel chan(arg->channel, RequestChannel::CLIENT_SIDE);
-  while(arg->b->numFinished < 3 || arg->b->getSize() != 0){
+  while(arg->b->numFinished < 3 || arg->b->getSize() > 0){
     Item i = arg->b->remove();
     if(i.getMessage() != "NULL" && i.getPerson() != 'n'){
       i.setData(chan.send_request(i.getMessage()));
       cout<<"\nResponse: "<<i.getData()<<i.getPerson()<<endl;
       if(i.getPerson() == 'j')
-        hist[0][atoi(i.getData().c_str())]++;
+        arg->b1->add(i);
       else if(i.getPerson() == 'l')
-        hist[1][atoi(i.getData().c_str())]++;
+        arg->b2->add(i);
       else if(i.getPerson() == 'g')
-        hist[2][atoi(i.getData().c_str())]++;
+        arg->b3->add(i); 
     }
   }
+  arg->b1->finished();
+  arg->b2->finished();
+  arg->b3->finished();
+  cout<<endl<<arg->b3->numFinished<<":"<<arg->b3->getSize()<<endl;
   chan.send_request("quit");
 }
-void *histogram(void *param){
-	Arguments* arg = (Arguments *)param;
-	
-}
-void printHistogram(){
-  for (int i = 0; i < 3; ++i)
-  {
+void printHistogram(int i){
     cout<<"Histogram: "<<i+1<<endl;
     for (int j = 0; j < 100; ++j)
     {
@@ -105,7 +106,23 @@ void printHistogram(){
         cout<<"*";
       cout<<endl;
     }
-  }
+}
+void *histogram(void *param){
+	Arguments* arg = (Arguments *)param;
+	while(arg->b->numFinished < 3 || arg->b->getSize() > 0){
+		if(arg->id == 'g')
+			cout<<endl<<arg->b->numFinished<<":"<<arg->b->getSize()<<endl;
+		 Item i = arg->b->remove();
+		 if(i.getMessage() != "NULL" && i.getPerson() != 'n'){
+			if(i.getPerson() == 'j')
+				hist[0][atoi(i.getData().c_str())]++;
+			else if(i.getPerson() == 'l')
+				hist[1][atoi(i.getData().c_str())]++;
+		   	else if(i.getPerson() == 'g')
+				hist[2][atoi(i.getData().c_str())]++;
+		 }
+	}
+	cout<<"\nDone!" <<arg->id<<endl;
 }
 
 int main(int argc, char * argv[]) {
@@ -122,7 +139,7 @@ int main(int argc, char * argv[]) {
       cout<<"Error, fork failed"<<endl;
     return -1;
   }
-  int w = 40;
+  int w = 3;
   vector<pthread_t> clients(3);
   vector<pthread_t> workers(w);
   vector<pthread_t> histWorkers(w);
@@ -147,24 +164,48 @@ int main(int argc, char * argv[]) {
   pthread_create(&clients[2], NULL, request, &arg3);
     //usleep(10000000);
   vector<Arguments> arr(w);
-  vector<BoundedBuffer> histBuffers;
-  for(int i =0; i < 3;++i)
-	histBuffers.push_back(BoundedBuffer(30,&s));
+  BoundedBuffer hist1(30,&s);
+  BoundedBuffer hist2(30,&s);
+  BoundedBuffer hist3(30,&s);
   for (int i = 0; i < w; ++i)
   {
     arr[i].channel = chan.send_request("newthread");
     arr[i].b = &b;
+	arr[i].b1 = &hist1;
+	arr[i].b2 = &hist2;
+	arr[i].b3 = &hist3;
     pthread_create(&workers[i], NULL, worker, &arr[i]);
   }
+  Arguments arg4;
+  arg4.id = 'j';
+  arg4.rep = w;
+  arg4.b = &hist1;
+  pthread_create(&histWorkers[0], NULL, histogram, &arg4);
+  Arguments arg5;
+  arg5.id = 'l';
+  arg5.rep = w;
+  arg5.b = &hist2;
+  pthread_create(&histWorkers[1], NULL, histogram, &arg5);
+  Arguments arg6;
+  arg6.id = 'g';
+  arg4.rep = w;
+  arg4.b = &hist3;
+  pthread_create(&histWorkers[2], NULL, histogram, &arg6);
+  
   for (int i = 0; i < clients.size(); ++i)
   {
     pthread_join(clients[i], NULL);
   }
-  for (int i = 0; i < 1; ++i)
+  for (int i = 0; i < w; ++i)
   {
     pthread_join(workers[i], NULL);
   }
+  for (int i = 0; i < 3; ++i)
+  {
+    pthread_join(histWorkers[i], NULL);
+  }
   chan.send_request("quit");
   usleep(1000000);
-  printHistogram();
+  for(int i = 0; i < 3; ++i)
+	  printHistogram(i);
 }
